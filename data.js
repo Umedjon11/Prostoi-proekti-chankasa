cffeeaeef0d42dba1579ed3552c37e1880492dff
Umedjon <nazarovumed828@gmail.com>
2025-08-26 10:32:19 +0500

    second

---
+++ data.js
@@ -0,0 +1,63 @@
+const projectsData = {
+    "Umed": [
+        {
+            name: "Home Page",
+            path: "Umed/Home Page/index.html",
+            image: "image/Banner.jpg"
+        },
+        {
+            name: "Home Page v.2",
+            path: "Umed/Home Page v.2/index.html",
+            image: "image/Banner (1).png"
+        },
+        {
+            name: "Product Listing v.2",
+            path: "Umed/Product Listing v.2/index.html",
+            image: "image/Banner(2).png"
+        },
+        {
+            name: "Product Listing v.3",
+            path: "Umed/Product Listing v.2/index.html",
+            image: "image/Banner.png"
+        }
+    ],
+    "Abdullo": [
+        {
+            name: "About Page",
+            path: "Abdullo/About page/index.html",
+            image: "images/umed_404.jpg"
+        },
+        {
+            name: "About page v.2",
+            path: "Abdullo/About page v.2/index.html",
+            image: "images/umed_blog_single.jpg"
+        },
+        {
+            name: "",
+            path: "Abdullo/Product Listing/index.html",
+            image: "images/umed_contact.jpg"
+        },
+        {
+            name: "Лицензии",
+            path: "UMED/Licences/index1.html",
+            image: "images/umed_licences.jpg"
+        },
+        {
+            name: "Защита паролем",
+            path: "UMED/Password Protect/index.html",
+            image: "images/umed_password_protect.jpg"
+        }
+    ],
+    "Idiboy": [
+        {
+            name: "Shoping baskets",
+            path: "Idiboy/Shoping baskets/index.html",
+            image: "images/ismoil_portfolio.jpg"
+        },
+        {
+            name: "Shoping baskets v.2",
+            path: "Idiboy/Shoping baskets/index.html",
+            image: "images/ismoil_portfolio_single.jpg"
+        }
+    ]
+};
